a2f20dbef63a26d9d49446bfb4f7305ad495dd66
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-09-05 14:31:14 +0300

    comment-controller-created

--- Project7-Project-Management-System/controllers/comment_controller.go
+++ Project7-Project-Management-System/controllers/comment_controller.go
@@ -1,1 +1,58 @@
 package controllers
+
+import (
+	"Project-Management-System/models"
+	"Project-Management-System/services"
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
+
+func CreateComment(c *fiber.Ctx) error {
+	var comment models.CommentModel
+	if err := c.BodyParser(&comment); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+	taskId := c.Params("task_id")
+	comment.TaskId = taskId
+	comment.CreatedAt = time.Now()
+
+	createdComment, err := services.CreateComment(comment)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	return c.Status(fiber.StatusCreated).JSON(createdComment)
+}
+
+func GetComments(c *fiber.Ctx) error {
+	taskId := c.Params("task_id")
+	comments, err := services.GetCommentsByTaskId(taskId)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	return c.Status(fiber.StatusOK).JSON(comments)
+}
+
+func UpdateComment(c *fiber.Ctx) error {
+	commentId := c.Params("comment_id")
+	var updatedComment models.CommentModel
+
+	if err := c.BodyParser(&updatedComment); err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	err := services.UpdateComment(commentId, updatedComment)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).SendString("comment not found")
+	}
+
+	return c.Status(fiber.StatusOK).JSON(updatedComment)
+}
+
+func DeleteComment(c *fiber.Ctx) error {
+	commentId := c.Params("comment_id")
+	err := services.DeleteComment(commentId)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).SendString("comment not found")
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.StatusNoContent)
+}
